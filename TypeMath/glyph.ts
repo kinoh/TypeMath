61b82f841f5be6fff70389690ebf441be6182abb
Kinon <segenswind@gmail.com>
2014-01-05 11:20:39 +0900

    improve glyph

--- TypeMath/glyph.ts
+++ TypeMath/glyph.ts
@@ -8,13 +8,15 @@ class Segment
 {
     p0: Point;
     p1: Point;
-    weight: number;
+	weight0: number;
+	weight1: number;
 
-	constructor(p0x: number, p0y: number, p1x: number, p1y: number, w: number)
+	constructor(p0x: number, p0y: number, p1x: number, p1y: number, w0: number, w1: number)
     {
 		this.p0 = { x: p0x, y: p0y };
 		this.p1 = { x: p1x, y: p1y };
-        this.weight = w;
+		this.weight0 = w0;
+		this.weight1 = w1;
 	}
 
 	public draw(ctx: CanvasRenderingContext2D): void
@@ -24,6 +26,11 @@ class Segment
 }
 class Line extends Segment
 {
+	constructor(p0x: number, p0y: number, p1x: number, p1y: number, w0: number, w1?: number)
+	{
+		super(p0x, p0y, p1x, p1y, w0, w1 !== undefined ? w1 : w0);
+	}
+
     public toString(): string
     {
         return "line " + this.p0.toString() + " " + this.p1.toString();
@@ -31,11 +38,11 @@ class Line extends Segment
 
 	public draw(ctx: CanvasRenderingContext2D): void
 	{
-		var w = this.weight / 2.0;
-		var d0, d1: Point;
-		
 		var t = Math.atan2(this.p1.y - this.p0.y, this.p1.x - this.p0.x) + Math.PI / 2.0;
-		d0 = d1 = { x: w * Math.cos(t), y: w * Math.sin(t) };
+		var w0 = this.weight0 / 2.0;
+		var w1 = this.weight1 / 2.0;
+		var d0 = { x: w0 * Math.cos(t), y: w0 * Math.sin(t) };
+		var d1 = { x: w1 * Math.cos(t), y: w1 * Math.sin(t) };
 
 		ctx.moveTo(this.p0.x + d0.x, this.p0.y + d0.y);
 		ctx.lineTo(this.p1.x + d1.x, this.p1.y + d1.y);
@@ -47,35 +54,31 @@ class Line extends Segment
 class Bezier extends Segment
 {
     c1: Point;
-    c2: Point;
-    w0: number;
-    w1: number;
+	c2: Point;
+	weight: number;
 
     constructor(p0x: number, p0y: number, p1x: number, p1y: number,
         c1x: number, c1y: number, c2x: number, c2y: number,
         w0: number, w1: number, w: number)
     {
-        super(p0x, p0y, p1x, p1y, w);
+        super(p0x, p0y, p1x, p1y, w0, w1);
 		this.c1 = { x: c1x, y: c1y };
 		this.c2 = { x: c2x, y: c2y };
-        this.w0 = w0;
-        this.w1 = w1;
+        this.weight = w;
     }
 
 	public draw(ctx: CanvasRenderingContext2D): void
 	{
 		var w = this.weight / 2.0;
-		var w0, w1: number;
-		var d0, d1, dc: Point;
+		var w0 = this.weight0 / 2.0;
+		var w1 = this.weight1 / 2.0;
 
 		var t0 = Math.atan2(this.c1.y - this.p0.y, this.c1.x - this.p0.x) + Math.PI / 2.0;
-		var t1 = Math.atan2(this.p0.y - this.c2.y, this.p0.x - this.c2.x) + Math.PI / 2.0;
-		var tc = Math.atan2(this.c2.y - this.c1.y, this.c2.x - this.c1.x) + Math.PI / 2.0;
-		w0 = this.w0 / 2.0;
-		w1 = this.w1 / 2.0;
-		d0 = { x: w0 * Math.cos(t0), y: w0 * Math.sin(t0) };
-		d1 = { x: w1 * Math.cos(t1), y: w1 * Math.sin(t1) };
-		dc = { x: w * Math.cos(tc), y: w * Math.sin(tc) };
+		var t1 = Math.atan2(this.c2.y - this.p1.y, this.c2.x - this.p1.x) - Math.PI / 2.0;
+		var tc = Math.atan2(this.p1.y - this.p0.y, this.p1.x - this.p0.x) + Math.PI / 2.0;
+		var d0 = { x: w0 * Math.cos(t0), y: w0 * Math.sin(t0) };
+		var d1 = { x: w1 * Math.cos(t1), y: w1 * Math.sin(t1) };
+		var dc = { x: w * Math.cos(tc), y: w * Math.sin(tc) };
 		
 		ctx.moveTo(this.p0.x + d0.x, this.p0.y + d0.y);
 		ctx.bezierCurveTo(this.c1.x + dc.x, this.c1.y + dc.y, this.c2.x + dc.x, this.c2.y + dc.y,
@@ -122,14 +125,33 @@ class Glyph
             if (s instanceof Bezier)
             {
                 var b = <Bezier> s;
-                r.seg.push(new Bezier(w - b.p0.x, b.p0.y, w - b.p1.x, b.p1.y, w - b.c1.x, b.c1.y, w - b.c2.x, b.c2.y, b.w0, b.w1, b.weight));
+                r.seg.push(new Bezier(w - b.p0.x, b.p0.y, w - b.p1.x, b.p1.y, w - b.c1.x, b.c1.y, w - b.c2.x, b.c2.y, b.weight0, b.weight1, b.weight));
             }
             else if (s instanceof Line)
-                r.seg.push(new Line(w - s.p0.x, s.p0.y, w - s.p1.x, s.p1.y, s.weight));
+                r.seg.push(new Line(w - s.p0.x, s.p0.y, w - s.p1.x, s.p1.y, s.weight0, s.weight1));
         }
 
         return r;
-    }
+	}
+	public turnRight(): Glyph
+	{
+		var r = new Glyph(this.height, this.width);
+
+		for (var i = 0; i < this.seg.length; i++)
+		{
+			var s = this.seg[i];
+
+			if (s instanceof Bezier)
+			{
+				var b = <Bezier> s;
+				r.seg.push(new Bezier(b.p0.y, b.p0.x, b.p1.y, b.p1.x, b.c1.y, b.c1.x, b.c2.y, b.c2.x, b.weight0, b.weight1, b.weight));
+			}
+			else if (s instanceof Line)
+				r.seg.push(new Line(s.p0.y, s.p0.x, s.p1.y, s.p1.x, s.weight0, s.weight1));
+		}
+
+		return r;
+	}
 }
 
 class GlyphFactory
@@ -144,10 +166,10 @@ class GlyphFactory
         this.canvas = canvas;
 
         this.data["("] = new Glyph(24, 64,
-            new Bezier(20, 4, 20, 60, 4, 2, 4, 62, 0, 0, 4));
+            new Bezier(20, 4, 20, 60, 4, 2, 4, 62, 0, 0, 3));
         this.data["{"] = new Glyph(24, 64,
-            new Bezier(20, 4, 4, 32, 2, 0, 16, 32, 0, 0, 5),
-            new Bezier(4, 32, 20, 60, 16, 32, 2, 64, 0, 0, 5));
+            new Bezier(20, 4, 4, 32, 2, 0, 16, 32, 0, 0, 4),
+            new Bezier(4, 32, 20, 60, 16, 32, 2, 64, 0, 0, 4));
         this.data["["] = new Glyph(24, 64,
             new Line(16, 4, 8, 4, 1),
             new Line(8, 4, 8, 60, 2.5),
@@ -158,11 +180,11 @@ class GlyphFactory
             new Line(9, 4, 9, 60, 2),
             new Line(16, 4, 16, 60, 2));
 		this.data["⌊"] = new Glyph(24, 64,
-			new Line(8, 4, 8, 60, 3),
+			new Line(8, 4, 8, 60, 2.5),
 			new Line(8, 60, 16, 60, 1));
 		this.data["⌈"] = new Glyph(24, 64,
 			new Line(16, 4, 8, 4, 1),
-			new Line(8, 4, 8, 60, 3));
+			new Line(8, 4, 8, 60, 2.5));
 		this.data["〈"] = new Glyph(24, 64,
 			new Line(16, 4, 8, 32, 2),
 			new Line(8, 32, 16, 60, 2));
---
+++ TypeMath/glyphEdit.js
@@ -0,0 +1,179 @@
+﻿var data = {};
+
+data["("] = new Glyph(24, 64,
+	new Bezier(20, 4, 20, 60, 4, 2, 4, 62, 0, 0, 3));
+data["{"] = new Glyph(24, 64,
+	new Bezier(20, 4, 4, 32, 2, 0, 16, 32, 0, 0, 4),
+	new Bezier(4, 32, 20, 60, 16, 32, 2, 64, 0, 0, 4));
+data["["] = new Glyph(24, 64,
+	new Line(16, 4, 8, 4, 1),
+	new Line(8, 4, 8, 60, 2.5),
+	new Line(8, 60, 16, 60, 1));
+data["widetilde"] = new Glyph(64, 24,
+	new Bezier(8, 16, 56, 8, 28, -6, 36, 30, 1, 1, 4));
+data["widehat"] = new Glyph(64, 24,
+	new Line(8, 16, 32, 8, 1, 3),
+	new Line(32, 8, 56, 16, 3, 1));
+data["overrightarrow"] = new Glyph(64, 24,
+	new Line(4, 12, 60, 12, 1, 1),
+	new Bezier(60, 12, 54, 6, 56, 12, 54, 10, 0, 0, 5),
+	new Bezier(60, 12, 54, 18, 56, 12, 54, 14, 0, 0, 5));
+data["overbrace"] = data["{"].turnRight();
+
+var glyph = data["("];
+
+var points = {};
+var index = "";
+var unizon = "";
+var hide = false;
+
+
+window.onload = function ()
+{
+	for (var g in data)
+	{
+		var s = document.createElement("option");
+		s.text = g;
+		_("glyph").add(s, null);
+	}
+
+	render();
+
+	_("c").onmousedown = function (e)
+	{
+		var r = { x: e.layerX, y: e.layerY };
+		var near = Object.keys(points)
+			.map(function (i) { return { i: i, d: pointLen(points[i], r) }; })
+			.reduce(function (prev, curr) { return (curr.d < prev.d ? curr : prev); });
+
+		if (near.d > 10)
+			return;
+
+		index = near.i;
+
+		var u = Object.keys(points)
+			.map(function (i) { return { p: points[i], i: i }; })
+			.filter(function (q) { q.i != index && pointEq(points[index], q.p) });
+		if (u.length == 1)
+			unizon = u[0].i;
+	}
+	_("c").onmouseup = function (e)
+	{
+		index = unizon = "";
+	}
+	_("c").onmousemove = function (e)
+	{
+		if (index == "")
+			return;
+
+		var q = { x: e.layerX, y: e.layerY };
+
+		var i = index.split("-");
+		glyph.seg[i[0]][i[1]] = q;
+		if (unizon != "")
+		{
+			var j = unizon.split("-");
+			glyph.seg[j[0]][j[1]] = q;
+		}
+
+		render();
+	}
+	_("glyph").onchange = function () { glyph = data[_("glyph").options[_("glyph").selectedIndex].text]; render(); };
+	_("output").onclick = output;
+	_("hide").onclick = function () { hide = !hide; render(); };
+}
+
+function render()
+{
+	var canvas = _("c");
+	canvas.width = glyph.width;
+	canvas.height = glyph.height;
+	var context = canvas.getContext("2d");
+	context.fillStyle = "#000";
+	context.strokeStyle = "#0c0";
+
+	for (var i = 0; i < glyph.seg.length; i++)
+	{
+		context.beginPath();
+		glyph.seg[i].draw(context);
+		context.fill();
+		if (!hide)
+			context.stroke();
+	}
+
+	drawInfo(context, glyph);
+}
+
+function output()
+{
+	var dat = _("c").toDataURL("image/png");
+
+	_("out").innerHTML = "<img src=\"" + dat + "\"/><br/>length: " + dat.length;
+}
+
+
+function drawInfo(ctx, p)
+{
+	points = {};
+	_("s").innerHTML = "";
+
+	for (var i = 0; i < p.seg.length; i++)
+	{
+		var s = p.seg[i];
+
+		if (!hide)
+		{
+			fillCircle(ctx, s.p0, "#00f");
+			fillCircle(ctx, s.p1, "#00f");
+		}
+		points[i + "-p0"] = s.p0;
+		points[i + "-p1"] = s.p1;
+
+		if (s instanceof Bezier)
+		{
+			if (!hide)
+			{
+				strokeLine(ctx, s.p0, s.c1, "#00f");
+				strokeLine(ctx, s.p1, s.c2, "#00f");
+				fillCircle(ctx, s.c1, "#f00");
+				fillCircle(ctx, s.c2, "#f00");
+			}
+			points[i + "-c1"] = s.c1;
+			points[i + "-c2"] = s.c2;
+			_("s").innerHTML += "Bezier " + s.p0.x + "," + s.p0.y + " to " + s.p1.x + "," + s.p1.y + "(" + s.c1.x + "," + s.c1.y + " " + s.c2.x + "," + s.c2.y + ")<br/>";
+		}
+		else if (s instanceof Line)
+		{
+			_("s").innerHTML += "Line " + s.p0.x + "," + s.p0.y + " to " + s.p1.x + "," + s.p1.y + "<br/>";
+		}
+	}
+
+}
+
+
+function _(id) { return document.getElementById(id); }
+
+function fillCircle(ctx, pos, color)
+{
+	ctx.beginPath();
+	ctx.fillStyle = color;
+	ctx.arc(pos.x, pos.y, 2, 0, 2 * Math.PI, false);
+	ctx.fill();
+}
+function strokeLine(ctx, from, to, color)
+{
+	ctx.beginPath();
+	ctx.strokeStyle = color;
+	ctx.moveTo(from.x, from.y);
+	ctx.lineTo(to.x, to.y);
+	ctx.stroke();
+}
+
+function pointLen(p1, p2)
+{
+	return Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2));
+}
+function pointEq(p1, p2)
+{
+	return p1.x == p2.x && p1.y == p2.y;
+}
